052b766adb70f5d05941f32f3bf4b4f8c718701c
Daniel Cornejo <contact@danielcornejo.dev>
2023-05-13 18:02:24 -0400

    movie details route and rendering

--- routes/index.js
+++ routes/index.js
@@ -16,5 +16,20 @@ router.get("/movies", (req, res)=>{
     });
 });
 
+router.get("/movies/:theID", (req, res)=>{
+    // when you have something with a : in the route
+    // that means the route will never go to 
+    //  /guitars/theID it will go to something like /guitars/14hfh253534
+    // whatever value actually gets put into the URL, will be accessible
+    // as req.params.theID
+    Movie.findById(req.params.theID)
+    .then((theMovie)=>{
+        res.render("movieDetails", {theMovie: theMovie})
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+});
+
 
 module.exports = router;
