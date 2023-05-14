b679e33cd4706a87b7a92d9a0618bce4c26dd697
Daniel Cornejo <contact@danielcornejo.dev>
2023-05-14 19:52:50 -0400

    create new movie complete, needs css styling

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,27 @@ router.get("/movies", (req, res)=>{
     });
 });
 
+router.get("/movies/new", (req, res, next)=>{
+    res.render("new-movie");
+ });
+
+router.post("/movies/create", (req, res)=>{
+    Movie.create({
+        title: req.body.title,
+        director: req.body.director,
+        stars: req.body.stars,
+        image: req.body.img,
+        description: req.body.description,
+        showtimes: req.body.showtimes
+    }).then((response)=>{
+        res.redirect("/movies")
+    }).catch((err)=>{
+        console.log(err);
+    })
+// on a post request, on the .then, you always redirect, never render
+});
+
+
 router.get("/movies/:theID", (req, res)=>{
     // when you have something with a : in the route
     // that means the route will never go to 
@@ -30,6 +51,8 @@ router.get("/movies/:theID", (req, res)=>{
         console.log(err);
     });
 });
+
+
 
 
 module.exports = router;
